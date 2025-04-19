b8b18897cd5b82ac37a38d09d8a0ada86bfa212f
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-04-19 16:48:35 +0200

    base with other way to do

--- base/base.ino
+++ base/base.ino
@@ -1,30 +1,27 @@
-#define LED_PIN   2  
-// Creer une variable, sans utiliser define
+// #include <Adafruit_NeoPixel.h>
+// #define LED_PIN 2
+// // Creer une variable, sans utiliser define
 // bool state = true;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.printf("started");
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(115200);
+//   Serial.printf("started");
 
-  // Afficher la variable dans port
+//   // Afficher la variable dans port
 
-  pinMode(LED_PIN, OUTPUT);
+//   pinMode(LED_PIN, OUTPUT);
+// }
 
-}
-
-void loop() {
-  // Allume la lumiere, en passant par la variable 
-    digitalWrite(LED_PIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_PIN, LOW);
-    delay(1000);
-    // digitalWrite(LED_PIN, state ? HIGH : LOW);
-    // delay(500);
-
-  // Recupere une nouvelle valeur dans le port serie (true, false)
-  // affiche la variable 
-  // modifie l'etat de la led en foncition
+// void loop() {
+//   // Allume la lumiere, en passant par la variable
+//   // digitalWrite(LED_PIN, HIGH);
+//   // delay(1000);
+//   // digitalWrite(LED_PIN, LOW);
+//   // delay(1000);
+//   digitalWrite(LED_PIN, state ? HIGH : LOW);
+//   delay(1000);
+//   state = !state;
+// }
 
 
-}
