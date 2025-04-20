bf8f407193534767037c357900b0937c15ee3243
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-04-20 20:56:32 +0200

    wifi with waves

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,11 +1,19 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include "secrets.h"
+#include <FastLED.h>
 
+#define NUM_LEDS 5
+#define DATA_PIN 2
+#define BRIGHTNESS 55
+bool isOn = true;
+
+CRGB leds[NUM_LEDS];
+uint8_t gHue = 0;
 
 
 // Adresse IP fixe
-IPAddress local_IP(192, 168, 0, 200);      // Adresse IP fixe désirée
+IPAddress local_IP(192, 168, 0, 200);      // Adresse IP fixe 
 IPAddress gateway(192, 168, 0, 1);         // IP de la box
 IPAddress subnet(255, 255, 255, 0);        // Masque de sous-réseau
 IPAddress primaryDNS(8, 8, 8, 8);          // DNS (optionnel)
@@ -36,11 +44,15 @@ void connectToWiFi() {
 
 void handleLedOn() {
   digitalWrite(LedBleuPin, HIGH);
+  isOn = true;
   server.send(200, "text/plain", "LED BLEUE ON");
 }
 
 void handleLedOff() {
   digitalWrite(LedBleuPin, LOW);
+  isOn = false;
+  FastLED.clear();      // éteint les LEDs RGB
+  FastLED.show();
   server.send(200, "text/plain", "LED BLEUE OFF");
 }
 
@@ -61,8 +73,21 @@ void setup() {
   server.begin();
 
   Serial.println("Serveur HTTP lancé !");
+
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
   server.handleClient();
+    if (isOn) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CHSV(gHue + i * 30, 255, 255);
+      FastLED.show();
+      delay(150);
+      leds[i] = CRGB::Black;
+    }
+    gHue += 10;
+  }
 }
+
