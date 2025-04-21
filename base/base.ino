fbeb637c174a2b03178b7dd86a3c3237cc9c08ba
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-04-21 10:06:56 +0200

    gitignore

--- base/base.ino
+++ base/base.ino
@@ -1,27 +1,26 @@
-// #include <Adafruit_NeoPixel.h>
-// #define LED_PIN 2
+#define LED_PIN 2
 // // Creer une variable, sans utiliser define
-// bool state = true;
+bool state = true;
 
-// void setup() {
+void setup() {
 //   // put your setup code here, to run once:
-//   Serial.begin(115200);
-//   Serial.printf("started");
+  Serial.begin(115200); //communication entre ESP32 et ordi
+  Serial.printf("started");
 
 //   // Afficher la variable dans port
 
-//   pinMode(LED_PIN, OUTPUT);
-// }
+  pinMode(LED_PIN, OUTPUT);
+}
 
-// void loop() {
+void loop() {
 //   // Allume la lumiere, en passant par la variable
 //   // digitalWrite(LED_PIN, HIGH);
 //   // delay(1000);
 //   // digitalWrite(LED_PIN, LOW);
 //   // delay(1000);
-//   digitalWrite(LED_PIN, state ? HIGH : LOW);
-//   delay(1000);
-//   state = !state;
-// }
+  digitalWrite(LED_PIN, state ? HIGH : LOW);
+  delay(1000);
+  state = !state;
+}
 
 
